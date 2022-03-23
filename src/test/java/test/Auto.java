23a4581e53c029503ea31fe4a953d4c420cb0c0d
Daniel Ceballos <dceballosm@unal.edu.co>
2022-03-22 22:22:16 -0500

    Create Auto.java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,36 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int numeroAsientos = 0;
+		for(int i = 0; i < asientos.length; i++) {
+			if (asientos[i] instanceof Asiento) {
+				numeroAsientos++;
+			}
+		}
+		return numeroAsientos;
+	}
+	
+	String verificarIntegridad() {
+		if (this.motor.registro == this.registro) {
+			for(int i = 0; i < asientos.length; i++) {
+				if(asientos[i] != null) {
+					if (asientos[i].registro != this.registro) {
+						return "Las piezas no son originales";
+					}
+				}
+			}
+			return "Auto original";
+		}
+		else {
+			return "Las piezas no son originales";
+		}
+	}
+}
